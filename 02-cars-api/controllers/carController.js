12f7abef0b535e2a0de57b5f2b50f0c5df83bf51
César Guerra <warderer@gmail.com>
2024-09-18 21:36:41 -0600

    ✨ Add MVC: DeleteCarById

--- 02-cars-api/controllers/carController.js
+++ 02-cars-api/controllers/carController.js
@@ -15,7 +15,7 @@ const createCar = async (req, res) => {
 // Get all cars
 const getAllCars = async (req, res) => {
   try {
-    const cars = await Car.find()
+    const cars = await Car.find({ isActive: true })
     res.status(200).json(cars)
   } catch (error) {
     res.status(400).json({ message: 'Error Getting Cars', error })
@@ -25,7 +25,7 @@ const getAllCars = async (req, res) => {
 // Get car by ID
 const getCarById = async (req, res) => {
   try {
-    const car = await Car.findById(req.params.carId)
+    const car = await Car.findById({ _id: req.params.carId, isActive: true })
     res.status(200).json(car)
   } catch (error) {
     res.status(400).json({ message: 'Error Getting Car', error })
@@ -44,9 +44,36 @@ const updateCarById = async (req, res) => {
 
 // Delete
 
+const deleteCarById = async (req, res) => {
+  // Borrado físico: Voy a comprobar si existe un query string llamado 'destroy' y si su valor es 'true' ?destroy=true
+  if (req.query.destroy === 'true') {
+    try {
+      const deletedCar = await Car.findByIdAndDelete(req.params.carId)
+      if (deletedCar === null) { // Valido si el carro existe
+        return res.status(404).json({ message: 'Car not found for Delete' })
+      }
+      return res.status(204).json()
+    } catch (error) {
+      return res.status(400).json({ message: 'Error Deleting Car', error })
+    }
+  }
+
+  // Borrado lógico: Actualizar isActive a false
+  try {
+    const updatedCar = await Car.findByIdAndUpdate(req.params.carId, { isActive: false }, { new: false })
+    if (updatedCar === null || updatedCar.isActive === false) { // Valido si el carro existe
+      return res.status(404).json({ message: 'Car not found for Delete' })
+    }
+    return res.sendStatus(204)
+  } catch (error) {
+    return res.status(400).json({ message: 'Error Deleting Car', error })
+  }
+}
+
 export {
   createCar,
   getAllCars,
   getCarById,
-  updateCarById
+  updateCarById,
+  deleteCarById
 }
