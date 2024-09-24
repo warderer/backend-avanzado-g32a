54ac995a025f04f71fdedd5162e2c2b854ba9c93
César Guerra <warderer@gmail.com>
2024-09-23 20:00:11 -0600

    ✨ Add MongoDB connection to server

--- 03-books-api/index.js
+++ 03-books-api/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { connect } from './config/database.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -7,6 +8,8 @@ app.use(express.json())
 
 // Aqui van las rutas
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} 🚀`)
+connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`API running on http://localhost:${PORT} 🚀`)
+  })
 })
