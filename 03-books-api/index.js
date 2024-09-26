b00a704ab96a3706f924fbbc6fd39e5d5423b932
César Guerra <warderer@gmail.com>
2024-09-25 21:01:57 -0600

    ✨ Morgan custom tokens

--- 03-books-api/index.js
+++ 03-books-api/index.js
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+// tokens personalizados de Morgan
+morgan.token('body', (req) => JSON.stringify(req.body))
+morgan.token('host', (req) => req.hostname)
+morgan.token('query', (req) => JSON.stringify(req.query))
+
+app.use(morgan(':host :method :url :status :query :res[content-length] - :response-time ms - :body'))
 
 // Aqui van las rutas
 app.use('/api/v1/books', bookRoutes)
